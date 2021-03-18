e93396711ddeef7e80ca7bbee26994859e8c875f
Derek Forgione <derekforgione@gmail.com>
2021-03-17 22:29:57 -0700

    Transfered functions to chunk.py

--- world/util.py
+++ world/util.py
@@ -1,27 +1,5 @@
 import math
 
-def nibble4(arr, index):
-    return arr[index//2] & 0x0F if index % 2 == 0 else (arr[index//2]>>4) & 0x0F
-
-def chunk_block_index(*args):
-    if len(args) == 3:
-        return args[1]*256+args[2]*16+args[0]
-    else:
-        return args[0][1]*256+args[0][2]*16+args[0][0]
-
-def chunk_index_bitsize(palette_size):
-    return max(math.ceil(math.log2(palette_size)), 4)
-
-def extract_index(full_index, palette_size, block_states):
-    bitsize = max(math.ceil(math.log2(palette_size)), 4)
-    #vpl = values per long
-    vpl = 64 // bitsize
-    mask = 2**bitsize-1
-    state_index = full_index // vpl
-    value_offset = (full_index % vpl) * bitsize
-    slot = block_states[state_index]
-    return (slot & (mask << value_offset)) >> value_offset
-
 def sbyte_to_byte(v):
     if 0 <= v < 128:
         return v
