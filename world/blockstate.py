ae1c7699b5a3ff753c6f3cec77f40b59062275c3
Derek Forgione <derekforgione@gmail.com>
2021-03-17 22:26:35 -0700

    Renamed functions
    find_state is now find
    register_state is now register

--- world/blockstate.py
+++ world/blockstate.py
@@ -3,7 +3,7 @@ This module is for the block state registry. All block states are registered her
 """
 
 class BlockState:
-    __slots__ == ('key', 'id', 'properties')
+    __slots__ = ('unique_key', 'id', 'properties')
     def __init__(self, id, properties):
         self.unique_key = object()
         self.id = id
@@ -12,14 +12,21 @@ class BlockState:
 _id_state_registry = dict()
 _key_state_registry = dict()
 
-def find_state(id, props):
-    if id in _state_registry:
+def find_state(id, props = {}):
+    """
+    Finds the state in the registry.
+    """
+    if id in _id_state_registry:
         for p in _id_state_registry[id]:
             if p.properties == props:
                 return p
     return _key_state_registry.get(id, None)
 
-def register_state(id : str, props):
+def register_state(id : str, props = None) -> BlockState:
+    """
+    First tries to find the state if it already exists, then creates it if it does not.
+    The return value is the BlockState that was registered.
+    """
     state = find_state(id, props)
     if state:
         return state
