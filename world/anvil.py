3f17e5225d7585e26c469db249a4fdbde55628df
Derek Forgione <derekforgione@gmail.com>
2021-03-18 15:19:08 -0700

    Worked on memory sectors in region file.

--- world/anvil.py
+++ world/anvil.py
@@ -1,16 +1,76 @@
 import os
+from os import path
 import gzip
 import zlib
 import io
+import numpy
+import math
+import bisect
 from . import nbt
 from . import chunk
 
+class Sector(object):
+    __slots__ = ('offset', 'count')
+    def __init__(self, offset, count):
+        self.offset = offset
+        self.count = count
+    
+    @property
+    def end(self) -> int:
+        return self.offset + self.count
+    
+    @property
+    def size(self) -> int:
+        return self.count * 4096
+    
+    @property
+    def file_offset(self):
+        return self.offset * 4096
+    
+    def __lt__(self, other):
+        return self.offset < other.offset
+    
+    def __gt__(self, other):
+        return self.offset > other.offset
+    
+    def __eq__(self, other):
+        return self.offset == other.offset
+
 class RegionFile:
 
-    __slots__ = ('filename')
+    __slots__ = ('filename','items','sectors')
 
     def __init__(self, filename : str):
         self.filename = filename
+        self.items = numpy.ndarray(shape=(1024), dtype=numpy.object_)
+        self.sectors = None
+        if path.isfile(filename):
+            with open(self.filename, 'rb') as f:
+                f.seek(0, os.SEEK_END)
+                size = f.tell()
+                f.seek(0)
+                if size < 8192 or (size % 4096) != 0:
+                    raise Exception('Invalid region file!')
+
+                sect_count = size // 4096
+
+                self.sectors = [Sector(0, 2)]
+                for i in range(1024):
+                    offset = int.from_bytes(f.read(3), byteorder='big', signed=False)
+                    sector_count = int.from_bytes(f.read(1), byteorder='big', signed=False)
+                    sector = Sector(offset, sector_count)
+                    self.items[i] = sector
+                    bisect.insort(self.sectors, sector)
+                self.sectors.sort(key = lambda v: v.offset)
+    
+    def get_free_sector(self, size_in_bytes):
+        sector_count = math.ceil(size_in_bytes / 4096)
+        for i in range(0, len(self.sectors) - 1):
+            dist_between = self.sectors[i+1].offset - self.sectors[i].end
+            if dist_between >= sector_count:
+                sector = Sector(self.sectors[i].end, sector_count)
+                bisect.insort(self.sectors, sector)
+                return sector
 
     def read_chunk(self, offsetX : int, offsetZ : int) -> chunk.Chunk:
         tag, _ = self.read_chunk_tag(offsetX, offsetZ)
