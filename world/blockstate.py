1d41eeb5bd1b7eb72c838d66d1c8255e4d2d8f45
Derek Forgione <derekforgione@gmail.com>
2021-03-18 05:01:00 -0700

    Removed block states from enum.

--- world/blockstate.py
+++ world/blockstate.py
@@ -16,21 +16,27 @@ class BlockState:
 _id_state_registry = dict()
 _key_state_registry = dict()
 
+_mc_namespace = 'minecraft:'
+
 def find(id, props = {}):
     """
     Finds the state in the registry.
     """
+    if not id.startswith(_mc_namespace) and ':' not in id:
+        id = 'minecraft:' + id
     if id in _id_state_registry:
         for p in _id_state_registry[id]:
             if p.properties == props:
                 return p
     return _key_state_registry.get(id, None)
 
-def register(id : str, props = None) -> BlockState:
+def register(id : str, props = {}) -> BlockState:
     """
     First tries to find the state if it already exists, then creates it if it does not.
     The return value is the BlockState that was registered.
     """
+    if not id.startswith(_mc_namespace) and ':' not in id:
+        id = 'minecraft:' + id
     state = find(id, props)
     if state:
         return state
@@ -42,6 +48,6 @@ def register(id : str, props = None) -> BlockState:
         _id_state_registry[id] = [state]
     return state
 
-class blocks(Enum):
-    air = register('minecraft:air')
-    bedrock = register('minecraft:bedrock')
+air = register('minecraft:air')
+bedrock = register('minecraft:bedrock')
+stone = register('minecraft:stone')
