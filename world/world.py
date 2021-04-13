f25a23c127957254284971b650f8a9ac431f8c8c
Derek <derekforgione@gmail.com>
2021-04-12 22:50:20 -0700

    Minor changes.

--- world/world.py
+++ world/world.py
@@ -2,16 +2,20 @@ from abc import ABC, abstractmethod
 from . import anvil
 
 
-"""
-#######################################################################
-The goal of this file is to provide a class for Minecraft worlds.
-There should be a class called World for worlds, and then we can have
-another class called ChunkManager.
-ChunkManager will load, save, create, and store chunks.
-The World class will "ask" the ChunkManager for a chunk, and the
-ChunkManager will return the chunk if it is already loaded, or it will
-load and return the chunk.
-"""
+
+#########################################################################
+# The goal of this file is to provide a class for Minecraft worlds.
+# There should be a class called World for worlds, and then we can have
+# another class called ChunkManager.
+# ChunkManager will load, save, create, and store chunks.
+# The World class will "ask" the ChunkManager for a chunk, and the
+# ChunkManager will return the chunk if it is already loaded, or it will
+# load and return the chunk.
+# If the chunk does not exist, the ChunkManager must then decide to
+# create a new chunk, and if it decides to create a new chunk, it must
+# decide how that chunk is created.
+# This leaves an opening for world generators.
+#########################################################################
 
 class ChunkManager(ABC):
     @abstractmethod
